81b39f78dba6d51b664fffbf70e57997a09ad869
ycordell <ycordell@student.21-school.ru>
2021-01-14 17:16:09 +0300

    ничего)

---
+++ ft_printf.h
@@ -0,0 +1,13 @@
+
+
+# ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+typedef struct s_flag
+{
+}			t_flag;
+
+int ft_printf(const char *, ...);
+void pars(char *format, ...);
+
+#endif
