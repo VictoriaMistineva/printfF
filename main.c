9aee284a98fc336032772371f1fee048ccb2380b
ycordell <ycordell@student.21-school.ru>
2021-01-24 20:43:05 +0300

    P V0.2 дз

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 #define TEST(PATTERN, ...)\
 	out = ft_printf(PATTERN, __VA_ARGS__);\
-    printf(" %d\n", out);\
+    printf("MY= %d\n", out);\
 	out = printf(PATTERN, __VA_ARGS__);       \
     printf(" %d\n", out);\
 	printf("---------\n");
@@ -11,11 +11,9 @@ int main()
 {
 	int		out;
 
-	TEST("|%s|", "Hello")
-	TEST("|%.3s|", "Hello")
-	TEST("|%5.3s|", "Hello")
-	TEST("|%-5.3s|", "Hello")
-	TEST("|%05.3s|", "Hello")
+	TEST("|%p|", NULL)
+	TEST("|%-35p|", &out)
+	TEST("|%35p|", &out)
 
 	return (0);
 }
--- src/ftp_p.c
+++ src/ftp_p.c
@@ -1,7 +1,49 @@
 # include "ft_printf.h"
 
-int	print_p(void *arg, t_flags *flags)
+static int 		put_width_p(int conv_len, t_flags *flags)
 {
+	int		width_additional;
+	int 	result;
+
+	result = 0;
+	width_additional = flags->width.value - conv_len;
+	while (width_additional-- > 0)
+	{
+		write(1, " ", 1);
+		result++;
+	}
+	return (result);
+}
+
+static int	get_conversion_len(void *arg, int num_len, t_flags *flags)
+{
+	int 	result;
+
+	result = num_len;
+	if (flags->precision.is_specified && flags->precision.value > num_len)
+		result = flags->precision.value;
+	else if (flags->precision.is_specified && flags->precision.value == 0)
+		result = 0;
+	return (result);
+}
+
+int			print_p(void *arg, t_flags *flags)
+{	char 	*number;
+	int		num_len;
+	int		conv_len;
+
+	number = ft_ultoa_base(arg,16, 0, &num_len);
+	conv_len = get_conversion_len(arg, num_len, flags);
+	if (flags->minus)
+	{
+		write(1, number, num_len);
+		conv_len += put_width_p(conv_len, flags);
+	}
+	else
+	{
+		conv_len += put_width_p(conv_len, flags);
+		write(1, number, num_len);
+	}
 	return (conv_len);
 }
 
